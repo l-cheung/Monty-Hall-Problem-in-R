6b839a1a8dbb9087c456f9a2781e497a7d7b773d
l-cheung <35081231+l-cheung@users.noreply.github.com>
2018-01-03 22:31:50 -0500

    Add files via upload

---
+++ Monty Hall in R.R
@@ -0,0 +1,71 @@
+# Monty Hall Function - Lilian Cheung
+# This function simulates n runs of the Monty Hall problem and graphs the results
+# You can choose n and whether your strategy is to switch doors (switch=TRUE) or not (switch=FALSE)
+# Copy and paste this function into R to define it
+
+Monty <- function(n=1000, switch=TRUE) {
+
+# Initialize the win vector, where we'll store wins
+win <- rep(NA, n)
+
+# Define the possible prizes and 'bad prizes' resulting in a loss
+prizes <- c("Car", "Goat1", "Goat2")
+badprizes <- c("Goat1", "Goat2")
+
+ # Simulation
+  for (i in 1:n) {
+    
+  # Player picks a door randomly
+    x <- sample(1:3, 1)
+  # Randomly assign each one of the prizes to a 'door' 
+    doorsetup <- sample(prizes,3)
+    
+  # Note: y is the Player's final prize choice, depending on whether Player always switches or always doesn't switch
+    
+    
+    
+    # Strategy 1: Player always switches from original choice to the door not revealed by the host
+    if(switch) {
+      if(doorsetup[x]=="Car") {
+        reveal <- sample(badprizes,1)           # If the player's original choice was Car, the host will reveal either Goat1 or Goat2
+        revealpos <- which(doorsetup==reveal)   # Location of door revealed by host
+      } else if (doorsetup[x]=="Goat1") {
+        revealpos <- which(doorsetup=="Goat2")
+      } else if (doorsetup[x]=="Goat2") {
+        revealpos <- which(doorsetup=="Goat1") }
+      y <- doorsetup[-c(x, revealpos)]
+    
+      
+      
+    # Strategy 2: Player always stays with original choice, so that no matter what the Host does, the player's final choice will be whatever's behind door x
+    } else if (!switch) {
+      y <- doorsetup[x]
+    }
+    
+    
+    
+    # Determine whether player won based on the value of y
+    if (y=="Car") {
+      win[i] <- 1
+    } else {win[i] <- 0}
+  }
+
+
+# Print & graph the results of simulation
+  data <- data.frame(result=c("loss","win"), relativefrequency=c(table(win))/n)
+  print(data)
+  ggplot(data, aes(x=result, y=relativefrequency)) + geom_bar(stat = "identity")
+}
+
+
+
+
+### Using the Monty function
+
+set.seed(123)
+
+# Strategy: You stick with your original door choice
+Monty(n=100000, switch=FALSE)
+
+# Strategy: You stick with your original door choice
+Monty(n=100000, switch=TRUE)
